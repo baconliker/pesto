5e331403f570421c6c84309f9944d0ea0a5372a1
Colin Baker <colin@colinbaker.me.uk>
2024-01-29 13:39:41 +0000

    Fixed an issue parsing the date from .igc files which was due to a change of file format

--- Geolocation/Geolocation/Files/Igc.cs
+++ Geolocation/Geolocation/Files/Igc.cs
@@ -68,17 +68,44 @@ namespace ColinBaker.Geolocation.Files
 
 		private void ProcessHRecord(string fileLine, Tracks.Track track)
 		{
-			// Check the sub-type
-			switch (fileLine.Substring(2, 3).ToUpper())
+			(string subTypeCode, string headerValue) = ParseHRecord(fileLine);
+
+			switch (subTypeCode)
 			{
 				case "DTE":
-					m_currentDate = DateTime.ParseExact(fileLine.Substring(5, 6), "ddMMyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AssumeUniversal | System.Globalization.DateTimeStyles.AdjustToUniversal);
+
+					m_currentDate = DateTime.ParseExact(headerValue.Substring(0, 6), "ddMMyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AssumeUniversal | System.Globalization.DateTimeStyles.AdjustToUniversal);
 					track.Date = m_currentDate;
 					break;
 			}
 		}
 
-        private void ProcessBRecord(string fileLine, Tracks.Track track)
+		private (string SubTypeCode, string HeaderValue) ParseHRecord(string fileLine)
+		{
+			string subTypeCode = fileLine.Substring(2, 3).ToUpper();
+			int colonIndex = fileLine.IndexOf(':');
+
+			string headerValue = string.Empty;
+
+			if (colonIndex == -1)
+			{
+				// This is required for some header records in the older format of IGC files (e.g. DTE, FXA) where there is no colon separating the value from the subtype code
+				// For example in the old format IGC files the DTE header would be:
+				// HFDTE070723
+				// In the new format it's:
+				// HFDTEDATE:101123,01
+				// (it also now includes a 'flight number of the day' counter)
+				headerValue = fileLine.Substring(5);
+			}
+			else if (fileLine.Length > (colonIndex + 1))
+			{
+				headerValue = fileLine.Substring(colonIndex + 1);
+			}
+
+			return (subTypeCode, headerValue.Trim());
+		}
+
+		private void ProcessBRecord(string fileLine, Tracks.Track track)
 		{
 			Tracks.Fix fix = new Tracks.Fix();
 
